73695caf08e0b061d65a37fc2a68737d0cab533b
Sanda Essorong <essorong47@gmail.com>
2026-07-04 02:25:27 +0530

    feat: add serial print tracking for blink count

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,9 +1,16 @@
+unsigned long blinkCount = 0;
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600); // Start serial communication at 9600 baud rate
 }
 void loop() {
   digitalWrite(13, HIGH);
   delay(500);
   digitalWrite(13, LOW);
   delay(500);
+
+  blinkCount++; // Increment counter by 1
+  Serial.print("Blink count: ");
+  Serial.println(blinkCount); // Print current count to monitor
 }
