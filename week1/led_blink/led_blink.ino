665abec4a4ecf13cb2b6b015f835a09d1f4ef1d3
Sanda Essorong <essorong47@gmail.com>
2026-07-04 02:26:30 +0530

    feat: add potentiometer dynamic speed adjustment

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,16 +1,19 @@
 unsigned long blinkCount = 0;
+const int potPin = A0; // Potentiometer wiper terminal connected to Analog Pin 0
 
 void setup() {
   pinMode(13, OUTPUT);
-  Serial.begin(9600); // Start serial communication at 9600 baud rate
+  Serial.begin(9600);
 }
 void loop() {
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(500);
+  int delayVal = analogRead(potPin); // Read raw analog data (0 to 1023)
 
-  blinkCount++; // Increment counter by 1
+  digitalWrite(13, HIGH);
+  delay(delayVal); // Use potentiometer reading as the delay time
+  digitalWrite(13, LOW);
+  delay(delayVal);
+
+  blinkCount++;
   Serial.print("Blink count: ");
-  Serial.println(blinkCount); // Print current count to monitor
+  Serial.println(blinkCount);
 }
